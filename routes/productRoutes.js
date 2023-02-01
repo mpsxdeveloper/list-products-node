f495bcbe119011237802c03e9403ac147cdd0a2b
mpsxdeveloper <98715891+mpsxdeveloper@users.noreply.github.com>
2023-02-01 13:03:27 -0400

    Create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const productController = require('../controllers/productController')
+
+const router = express.Router()
+
+// list
+router.get("/", productController.product_list)
+// create
+router.get('/product', productController.product_create)
+// save
+router.post('/product', productController.product_save)
+// read
+router.get('/product/:id', productController.product_read)
+// update
+router.put('/product', productController.product_update)
+// delete
+router.get('/product/delete/:id', productController.product_delete)
+// search
+router.get('/product/search/:slug', productController.product_search)
+
+module.exports = {
+    router
+}
